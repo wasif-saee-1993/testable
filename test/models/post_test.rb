aa0f5ddd6673c743606426911905a4348c19f366
Wasif Saeed(2116) <wasif.prosku@nextbridge.com>
2025-05-06 16:12:23 +0500

    Post scaffold with test

---
+++ test/models/post_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class PostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "Should not save post without title" do
+    post = Post.new
+    assert_not post.save, "Saved post without title"
+  end
+
+  test "Should save post with title" do
+    post = Post.new(title: "Test")
+
+    assert post.save, "Saved with ID title"
+  end
+end
